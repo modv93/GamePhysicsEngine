3d84c3de3264358f4e0cbe00d30251c6b7a828c8
Shubham <shubham.tapmod@gmail.com>
2019-02-18 23:08:58 +0530

    03 Newton's first law completed and line trace for force vectors added

--- GamePhyicsEngine/Assets/Scripts/PhysicsEngine.cs
+++ GamePhyicsEngine/Assets/Scripts/PhysicsEngine.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PhysicsEngine : MonoBehaviour {
 	public Vector3 velocityVector;
+	public Vector3 netForceVector;
+	public List<Vector3> forceVectorList = new List<Vector3>();
 	// Use this for initialization
 	void Start () {
 		
@@ -14,7 +16,19 @@ public class PhysicsEngine : MonoBehaviour {
 		
 	}
 	void FixedUpdate() {
-		gameObject.transform.position = gameObject.transform.position + (velocityVector * Time.deltaTime);
+		AddForce ();
+		if (netForceVector == Vector3.zero) {
+			gameObject.transform.position = gameObject.transform.position + (velocityVector * Time.deltaTime);
+		} 
+		else {
+			Debug.LogError("Unbalanced force detected! ");
+		}
+			
 	}
-
+	void AddForce() {
+		netForceVector = Vector3.zero;
+		foreach (Vector3 forceVector in forceVectorList) {
+			netForceVector += forceVector;
+		}
+	}
 }
