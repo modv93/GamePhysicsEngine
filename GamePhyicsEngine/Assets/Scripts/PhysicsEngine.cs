9a941bd35bd37280a300cb6caec6b5a25a529e24
Shubham <shubham.tapmod@gmail.com>
2019-02-18 21:02:56 +0530

    02 Required files imported and script created for initial velocity

---
+++ GamePhyicsEngine/Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour {
+	public Vector3 velocityVector;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate() {
+		gameObject.transform.position = gameObject.transform.position + (velocityVector * Time.deltaTime);
+	}
+
+}
