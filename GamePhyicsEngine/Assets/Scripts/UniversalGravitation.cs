5abb3bbe2579ddf0476af2eff062a362c932eb28
Shubham <shubham.tapmod@gmail.com>
2019-03-12 03:46:21 +0530

    09 Universal Gravitation added for script execution order and appropriate force calculation

---
+++ GamePhyicsEngine/Assets/Scripts/UniversalGravitation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalGravitation : MonoBehaviour {
+
+	private const float gravitationConstant = 6.674e-11f; // [m^3 Kg^-1 ms^-2]
+	private PhysicsEngine[] physicsEngineArray;
+
+	void Start () {
+		physicsEngineArray = FindObjectsOfType<PhysicsEngine> ();
+	}
+
+
+	void FixedUpdate () {
+		CalculateGravity ();
+	}
+
+	void CalculateGravity() {
+		foreach (PhysicsEngine physicsEngineA in physicsEngineArray) {
+			foreach (PhysicsEngine physicsEngineB in physicsEngineArray) {
+				if (physicsEngineA != physicsEngineB && physicsEngineA != this) {
+					Vector3 distance = physicsEngineA.transform.position - physicsEngineB.transform.position;
+					float rSquared = Mathf.Pow (distance.magnitude, 2f);
+					float gravityMagnitude = gravitationConstant * physicsEngineA.mass * physicsEngineB.mass / rSquared;
+					Vector3 gravityFeltVector = gravityMagnitude * distance.normalized;
+					physicsEngineA.AddForces (-gravityFeltVector);
+				}
+			}
+		}
+	}
+}
