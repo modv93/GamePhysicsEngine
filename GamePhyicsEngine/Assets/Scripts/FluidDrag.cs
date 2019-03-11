eeb4a57fb4dffd888ac76617854d70491b76ad40
Shubham <shubham.tapmod@gmail.com>
2019-03-12 03:08:45 +0530

    08 Air resistance and physics drag component setup

---
+++ GamePhyicsEngine/Assets/Scripts/FluidDrag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidDrag : MonoBehaviour {
+
+	[Range(1, 2f)]
+	public float velocityExponent;
+	public float dragConstant;
+
+	private PhysicsEngine physicsEngine;
+
+	void Start () {
+		physicsEngine = GetComponent<PhysicsEngine> ();
+	}
+
+	void FixedUpdate(){
+		Vector3 velocityVector = physicsEngine.velocityVector;
+		float speed = velocityVector.magnitude;
+		float dragSize = CalculateDrag(speed);
+		Vector3 dragVector = dragSize * velocityVector.normalized;
+		physicsEngine.AddForces (-dragVector); // dragVector is applying in the opposite direction	
+	}
+	float CalculateDrag(float speed) {
+		return dragConstant * Mathf.Pow(speed, velocityExponent);
+	}
+}
